e66712f6dd8fa5fc7c7f46ff59e6955c43964c98
Facundo Olano <facundo.olano@gmail.com>
2021-04-08 11:36:17 -0300

    generic sharebot

--- bonobot/basebot.py
+++ bonobot/basebot.py
@@ -1,10 +1,14 @@
+import os
 import random
 
 import requests
 
+
 class BaseBot():
-    def __init__(self, token, icon_emoji, username):
-        self.token = token
+    def __init__(self, name, icon_emoji, username):
+        self.name = name
+        self.api_token = os.environ.get('SLACK_API_TOKEN')
+        self.bot_token = os.environ.get('SLACK_BOT_TOKEN')
         self.icon_emoji = icon_emoji
         self.username = username
 
@@ -17,14 +21,14 @@ class BaseBot():
         raise NotImplementedError
 
     def _bot_request(self, resource, **data):
-        headers = {'Authorization': 'Bearer ' + self.token}
+        headers = {'Authorization': 'Bearer ' + self.bot_token}
         requests.post('https://slack.com/api/' + resource,
                       json=data, headers=headers)
 
 
 class FileBot(BaseBot):
-    def __init__(self, token, icon_emoji, username, source_file):
-        super().__init__(token, icon_emoji, username)
+    def __init__(self, name, icon_emoji, username, source_file):
+        super().__init__(name, icon_emoji, username)
 
         with open(source_file) as f:
             self.messages = f.read().splitlines()
