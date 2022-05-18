b329f3c3b7944c093c6199cd8278305e09039530
Facundo Olano <facundo.olano@gmail.com>
2022-05-18 12:05:32 -0300

    multichannel

--- bonobot/haiku.py
+++ bonobot/haiku.py
@@ -1,24 +1,26 @@
 import random
 import requests
 import os
-from syltippy import syllabize
 import re
 
 
-def get_messages(channel_name, limit=100):
-    channel = get_channel_id(channel_name)
-    messages = []
-    cursor = None
-    while True:
-        resp = slack_request('conversations.history',
-                             channel=channel, cursor=cursor)
-        messages += [msg['text'] for msg in resp['messages']]
+def get_messages(channels, limit=100):
+    result = []
+    for channel_name in channels:
+        messages = []
+        channel = get_channel_id(channel_name)
+        cursor = None
+        while True:
+            resp = slack_request('conversations.history',
+                                 channel=channel, cursor=cursor)
+            messages += [msg['text'] for msg in resp['messages']]
 
-        if not resp['has_more'] or len(messages) > limit:
-            break
-        cursor = resp['response_metadata'].get('next_cursor')
+            if not resp['has_more'] or len(messages) > limit:
+                break
+            cursor = resp['response_metadata'].get('next_cursor')
 
-    return messages
+        result += messages
+    return result
 
 
 def get_channel_id(channel_name):
@@ -32,36 +34,16 @@ def slack_request(resource, **params):
     return resp.json()
 
 
-# def extract_phrases(messages):
-#     five = []
-#     seven = []
-#     for message in messages:
-#         # FIXME remove :something: and <something>
-#         message = re.sub("[\<\:].*?[\>\:]", "", message)
-#         phrases = message.replace('.', '\n').replace(';', '\n').split('\n')
-#         for phrase in phrases:
-#             phrase = phrase.strip()
-#             syllables = len(syllabize(phrase)[0])
-#             if syllables == 5:
-#                 five.append(phrase)
-#             elif syllables == 7:
-#                 seven.append(phrase)
-#     return five, seven
-
-
-# def random_haiku(five, seven):
-#     return '\n'.join([random.choice(five), random.choice(seven), random.choice(five)])
-
 def extract_phrases(messages):
     results = set()
     for message in messages:
-        # FIXME properly remove links
-        # remove :something: and <something>
+        # remove :emojis:
         message = re.sub(":[^\s]+:", "", message)
         phrases = message.split('\n')
         for phrase in phrases:
             phrase = phrase.strip()
             if '//' in phrase or '<' in phrase:
+                # no links, no mentions
                 continue
             elif 1 < len(phrase.split(' ')) < 8:
                 results.add(phrase)
@@ -70,6 +52,4 @@ def extract_phrases(messages):
 
 
 def random_haiku(phrases):
-    # phrases = sorted([random.choice(phrases), random.choice(phrases), random.choice(phrases)], key=len)
-    # return '\n'.join([phrases[1], phrases[2], phrases[0]])
     return '\n'.join([random.choice(phrases), random.choice(phrases), random.choice(phrases)])
